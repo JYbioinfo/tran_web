f4476f8ec4096fa9e0d2e3d034667fbc18235f51
wubo <350181947@qq.com>
2015-12-14 14:58:28 +0800

    view层5

---
+++ web/static/js/taskdetail.js
@@ -0,0 +1,13 @@
+/**
+ * Created by guhongjie on 2015/12/14.
+ */
+function save()
+{
+    name_zh = document.getElementById("name_zh").value
+    text_zh = document.getElementById("text_zh").value
+}
+function submit()
+{
+    name_zh = document.getElementById("name_zh").value
+    text_zh = document.getElementById("text_zh").value
+}
