0d5b52dd0787e24ece3a4190541d8740d6fb90c5
wang <786913256@qq.com>
2016-01-08 15:34:27 +0800

    2

--- tools/hmac_passwd.py
+++ tools/hmac_passwd.py
@@ -21,13 +21,22 @@ class HmacPasswd:
 def insert_user(account,password,user_right):
     db = DB()
     passwd = HmacPasswd(password).get_hmacpassed()
-    select_sql = "SELECT max(user_no) FROM account_for_disease;"
-    re = db.execute(select_sql)
-    user_no = db.fetchone()[0] + 1
-    insert_sql = "INSERT INTO account_for_disease (user_no,account,password,user_right) " \
-                 "VALUES ('%s','%s','%s','%s');" % (user_no,account,passwd,user_right)
-    db.execute(insert_sql)
-    print "Insert Success"
+    select_sql = "SELECT user_no FROM account_for_disease WHERE account = '%s';" % account
+    re1 = db.execute(select_sql)
+    if re1 > 0:
+        sys_no = db.fetchone()[0]
+        update_sql = "UPDATE account_for_disease SET password = '%s',user_right = '%s' " \
+                     "WHERE account = '%s' AND user_no = '%s';" % (passwd,user_right,account,sys_no)
+        db.execute(update_sql)
+        print "UPDATA Success"
+    else:
+        select_sql = "SELECT max(user_no) FROM account_for_disease;"
+        re = db.execute(select_sql)
+        user_no = db.fetchone()[0] + 1
+        insert_sql = "INSERT INTO account_for_disease (user_no,account,password,user_right) " \
+                     "VALUES ('%s','%s','%s','%s');" % (user_no,account,passwd,user_right)
+        db.execute(insert_sql)
+        print "Insert Success"
 
 if __name__ == '__main__':
-    insert_user("wubo","admin",0)
+    insert_user("wubo","admin1",1)
