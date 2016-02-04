457d503a9ca400d10734dec98ed9b2600b7b12d9
wang <786913256@qq.com>
2016-02-04 11:34:33 +0800

    2

--- api/api_task.py
+++ api/api_task.py
@@ -535,7 +535,7 @@ def checker_update(sys_no):
             disease_name_zn = disease_name_zn.replace("'","\\'")
             text_zn = text_zn.replace("'","\\'")
             update_sql = "UPDATE disease_detail SET disease_name_zn = '%s',text_zn = '%s' " \
-                         "WHERE sys_no = %d:" % \
+                         "WHERE sys_no = %d;" % \
                          (disease_name_zn,text_zn,sys_no)
             re2 = db.execute(update_sql)
             if re2 > 0:
